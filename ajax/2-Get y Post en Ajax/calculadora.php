fc7069eb7fc74849cd21817629d4b01872091df9
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2022-12-07 02:12:42 -0300

    04-03/28
    Get y Post en Ajax

---
+++ ajax/2-Get y Post en Ajax/calculadora.php
@@ -0,0 +1,7 @@
+<?php
+
+$porcinco = $_GET ['numero'] *5; //En la variable "porcinco" se alojara el valor que aloje el input de ID 'numero' de metodo 'get'. Cambiamos de get a post para que funcione. 
+
+echo $porcinco; //Mostrara el valor en pantalla.
+
+?>
