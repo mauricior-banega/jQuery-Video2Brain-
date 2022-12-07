2ca94604a687fbd5402af8d26b90d168e58f9641
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2022-12-07 00:38:55 -0300

    04-03/27
    Ajax: Load

---
+++ ajax/colores.php
@@ -0,0 +1,18 @@
+<?php
+
+if($_GET['color'] == "verde")
+{   
+    echo "El color verde es muy relajante";
+}
+
+if($_GET['color'] == "azul")
+{   
+    echo "El color verde es muy frio";
+}
+
+if($_GET['color'] == "rojo")
+{   
+    echo "El color verde es muy pasional";
+}
+
+?>
